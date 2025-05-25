dc0b29ba4eff8d0dec895a0f922aaf77d04e94a9
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-05-25 14:51:38 -0500

    Finalización de la pagina nosotros.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import { Product } from "./pages/Products/Product";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Product />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./AboutUs.css";
+import HeroSection from "./HeroSection/HeroSection";
+import AboutContent from "./AboutContent/AboutContent";
+import SectionBlock from "../../components/SectionBlock/SectionBlock";
+import VisionSection from "../../components/VisionSection/VisionSection";
+import FloatingButton from "../../components/FloatingButton/FloatingButton";
+
+const AboutUs = () => {
+  return (
+    <>
+      <HeroSection />
+      <AboutContent />
+
+      <SectionBlock
+        title="Nuestra Misión"
+        className="mission-section"
+        style={{ marginBottom: "100px" }}
+      >
+        "Conservar la precisión del tiempo que llevas contigo, ofreciendo{" "}
+        <span className="gold-text">servicios de excelencia</span> y productos
+        de la más alta calidad para garantizar el funcionamiento impecable de
+        tus relojes. Nos comprometemos a ser tu aliado relojero de confianza:{" "}
+        <strong>cercano</strong> cuando nos necesites,{" "}
+        <strong>meticuloso</strong> en cada detalle y <strong>constante</strong>{" "}
+        en nuestro afán por superar tus expectativas."
+      </SectionBlock>
+
+      <VisionSection>
+        "Ser reconocidos como la empresa líder en{" "}
+        <span className="gold-text">relojería de alta gama</span> en el mercado
+        nacional e internacional, destacándonos por nuestra{" "}
+        <strong>excelencia en el servicio</strong>,{" "}
+        <strong>compromiso con la precisión</strong> y una{" "}
+        <strong>atención personalizada</strong> que crea vínculos duraderos con
+        cada cliente. Aspiramos a transformar cada reloj que reparamos o
+        vendemos en un símbolo de{" "}
+        <span className="gold-text">
+          elegancia, confianza y valor atemporal
+        </span>
+        ."
+      </VisionSection>
+      <FloatingButton />
+    </>
+  );
+};
+
+export default AboutUs;
