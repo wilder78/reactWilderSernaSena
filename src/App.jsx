b43473221b675ede48bc921abe9345c01c0d4c7d
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-06-13 21:06:49 -0500

    Migración a la carpeta pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import Navbar from "./features/inventory/components/Header/Navbar";
-import Footer from "./features/inventory/components/Footer/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import { Product } from "./pages/Products/Product";
-import AboutUs from "./pages/AboutUs/AboutUs";
-import StorageSection from "./pages/StorageSection/StorageSection";
-import Contacts from "./pages/Contact/Contacts";
+
+import Pages from "./pages/Pages";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/despacho-mercancia" element={<StorageSection />} />
-        <Route path="/contact" element={<Contacts />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <Pages />;
 }
 
 export default App;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,27 @@
+// src/pages/Pages.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../features/inventory/components/Header/Navbar";
+import Footer from "../features/inventory/components/Footer/Footer";
+import Home from "./Home/Home";
+import { Product } from "./Products/Product";
+import AboutUs from "./AboutUs/AboutUs";
+import StorageSection from "./StorageSection/StorageSection";
+import Contacts from "./Contact/Contacts";
+
+function Pages() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/despacho-mercancia" element={<StorageSection />} />
+        <Route path="/contact" element={<Contacts />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default Pages;
