4f1c6a8edc811ba579081723b8214e6b3702207f
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-06-21 18:26:46 -0500

    modification: Add BrandAndPhoto component

---
+++ src/shared/components/BrandAndPhoto.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+// Estilos Swiper
+import "swiper/css";
+import "swiper/css/autoplay";
+
+function BrandAndPhoto({ images = [] }) {
+  if (!Array.isArray(images) || images.length === 0) {
+    return <p className="text-center">No hay imágenes disponibles.</p>;
+  }
+
+  return (
+    <div className="context">
+      <Swiper
+        modules={[Autoplay]}
+        slidesPerView={5}
+        spaceBetween={10}
+        loop={true}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          480: { slidesPerView: 3 },
+          768: { slidesPerView: 4 },
+          1024: { slidesPerView: 5 },
+        }}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="card">
+              <img src={image.src} alt={image.alt || `Slide ${index + 1}`} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default BrandAndPhoto;
